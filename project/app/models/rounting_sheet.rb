1dcb2ef19255caa36012fbac6ec8b8d04862f58c
Piul <raulbeni@gmail.com>
2012-03-13 22:50:12 -0300

    se solucionan algunos errores de programación. y se arregla algo con el migrate

--- project/app/models/rounting_sheet.rb
+++
@@ -1,5 +0,0 @@
-class RountingSheet < ActiveRecord::Base
-  belongs_to :areas
-  belongs_to :employees
-  has_many :rounting_sheet_details
-end
--- project/app/models/rounting_sheet_detail.rb
+++
@@ -1,6 +0,0 @@
-class RountingSheetDetail < ActiveRecord::Base
-  # set_primary_keys :id, :rounting_sheet_id
-  belongs_to :reasons
-  belongs_to :packages
-  belongs_to :rounting_sheets
-end
