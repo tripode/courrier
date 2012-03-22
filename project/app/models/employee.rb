ebff105643c337b08a2a32afa3af3d71ab6562fa
Piul <raulbeni@gmail.com>
2012-03-21 23:35:38 -0300

    se mejora la interfaz de Empleados

--- project/app/models/employee.rb
+++ project/app/models/employee.rb
@@ -6,9 +6,23 @@ class Employee < ActiveRecord::Base
   has_many :routing_sheets, :dependent => :destroy
   
   
+  
+  def buscarFunctionType
+    FunctionType.all.collect{|tp|[tp.description, tp.id]}
+  end
+  
 
   def format_birthday
-    birthday.strftime("%d/%m/%Y") if birthday
+    birthday.strftime("%d-%m-%Y") if birthday
     
   end
+  def format_admission_date
+    admission_date=Date.today
+    admission_date.strftime("%d-%m-%Y") if admission_date
+    
+    
+  end
+  
+  
+    
 end
