1793cc696540a02ee9e6cae8c69346af03538b3d
Piul <raulbeni@gmail.com>
2012-03-27 20:15:47 -0300

    Se agrega la interfaz Guia de transporte. Totalmente funcional. Falta aun validaciones, interfaz mas amigable, agregar varios campos con select. Pero la interfaz para pruebas es genial

--- project/app/models/transport_guide_detail.rb
+++ project/app/models/transport_guide_detail.rb
@@ -1,5 +1,5 @@
 class TransportGuideDetail < ActiveRecord::Base
-  self.primary_keys :id, :transport_guide_id
+#  self.primary_keys :id, :transport_guide_id
   belongs_to :transport_guide
   belongs_to :product_type
 end
