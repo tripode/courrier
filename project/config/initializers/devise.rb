d7ae4ea907de054a7eb12273b604cd738330dccb
wildo <setwildo31@gmail.com>
2012-04-07 14:33:04 -0430

    devise

--- project/config/initializers/devise.rb
+++ project/config/initializers/devise.rb
@@ -2,9 +2,7 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   
-  devise :database_authenticatable, :registerable,
-       :recoverable, :rememberable, :trackable, 
-       :validatable, :authentication_keys => [:login]
+  config.authentication_keys = [ :login ]
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
