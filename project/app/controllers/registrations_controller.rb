e27419a670655d0e6a5195dc0e857116f7b685f0
unknown <Karine Linka@KarineLinka-PC.(none)>
2012-04-11 22:13:15 -0400

    #69 fixed

--- project/app/controllers/registrations_controller.rb
+++ project/app/controllers/registrations_controller.rb
@@ -28,9 +28,27 @@ class RegistrationsController < Devise::RegistrationsController
     end
 
  
-  def create
-    super
+ def create
+    build_resource
+
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        #sign_in(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
   end
+  #def create
+  # super
+  #end
 
   def update
     super
@@ -44,5 +62,11 @@ class RegistrationsController < Devise::RegistrationsController
     @deleted_user.destroy
   end
 
+  protected
+  
+  
+  def after_sign_up_path_for(resource)
+    new_user_registration_path
+  end 
   
 end 
