0446a3e0ad316718e24f3695ac48f921ee5450a1
unknown <Karine Linka@KarineLinka-PC.(none)>
2012-04-14 20:52:35 -0400

    Issue #30 Create Model,View Controller: Receivers

--- project/db/migrate/20120330221439_create_users_have_and_belongs_to_many_roles.rb
+++ project/db/migrate/20120330221439_create_users_have_and_belongs_to_many_roles.rb
@@ -4,6 +4,10 @@ class CreateUsersHaveAndBelongsToManyRoles < ActiveRecord::Migration
     create_table :roles_users, :id => false do |t|
       t.references :role, :user
     end
+    
+    add_foreign_key :roles_users, :roles
+    add_foreign_key :roles_users, :users
+    
   end
  
   def self.down
--- project/db/migrate/20120405004159_add_employee_id_to_users.rb
+++ project/db/migrate/20120405004159_add_employee_id_to_users.rb
@@ -1,6 +1,6 @@
 class AddEmployeeIdToUsers < ActiveRecord::Migration
   def change
     add_column :users, :employee_id, :integer
-
+    add_foreign_key :users, :employees
   end
 end
