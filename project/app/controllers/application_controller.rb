d33abe50f5871d7d55af40fb1ee703514556a59a
Piul <raulbeni@gmail.com>
2012-04-15 21:59:16 -0400

    issue #77 se agrega el placeholder para autocomplit. mas cosas del issue #49

--- project/app/controllers/application_controller.rb
+++ project/app/controllers/application_controller.rb
@@ -8,6 +8,23 @@ class ApplicationController < ActionController::Base
     flash[:error] = "No tienes permisos para acceder a esta pagina."
     redirect_to root_url
   end
+
+  rescue_from ActiveRecord::RecordNotFound ,  :with => :invalid_method
+  rescue_from ActionController::RoutingError, :with => :route_not_found
+  rescue_from ActionController::MethodNotAllowed, :with => :invalid_method
+
+  private
+
+  def route_not_found
+    render :text => 'What the fuck are you looking for ?', :status => :not_found
+  end
+
+  def invalid_method
+#    message = "Now, did your mom tell you to #{request.request_method.to_s.upcase} that ?"
+#    render :text => message, :status => :method_not_allowed
+    flash[:error] = "No tienes permisos para acceder a esta pagina."
+    redirect_to root_url
+  end
   
   
   
