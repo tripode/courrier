38d858defa09a4b59b79cb704abf144b0614b1a4
unknown <Karine Linka@KarineLinka-PC.(none)>
2012-04-22 21:06:25 -0400

    quitar vestigios de antigua implementacion de mensajes.

--- project/app/controllers/application_controller.rb
+++ project/app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   
   #Para tener acceso a cualquier metodo en el controller 
   # debe haber un user loggeado.
-  before_filter :authenticate_user!, :errase_message
+  before_filter :authenticate_user!
   
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = "No tienes permisos para acceder a esta pagina."
