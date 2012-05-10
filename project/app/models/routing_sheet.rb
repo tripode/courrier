a3280ad7c9fef639d06e2ac92f5213777f8aeb73
wildo <setwildo31@gmail.com>
2012-05-09 22:28:11 -0430

    Issue #112 Hoja de Ruta: Agregar Validaciones

--- project/app/models/routing_sheet.rb
+++ project/app/models/routing_sheet.rb
@@ -3,6 +3,8 @@ class RoutingSheet < ActiveRecord::Base
   belongs_to :area
   belongs_to :employee
   
+  validates :number, :area_id, :routing_sheet_state_id, :employee_id, :date, :total_amount, :presence =>true
+  
   def format_admission_date
     admission_date=Date.today
     admission_date.strftime("%d-%m-%Y") if admission_date
--- project/app/models/routing_sheet_detail.rb
+++ project/app/models/routing_sheet_detail.rb
@@ -1,4 +1,6 @@
 class RoutingSheetDetail < ActiveRecord::Base
   belongs_to :product
   belongs_to :reason
+  
+  validates :routing_sheet_id, :product_id, :presence => true
 end
