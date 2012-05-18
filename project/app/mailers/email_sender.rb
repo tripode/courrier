9cf3aa4d5f6f7a303cc06c40452346c0279b9ee1
Leois <leoislinka@gmail.com>
2012-05-18 19:43:57 -0400

    #103: Cambios en la vista de email

--- project/app/mailers/email_sender.rb
+++ project/app/mailers/email_sender.rb
@@ -5,13 +5,19 @@ class EmailSender < ActionMailer::Base
   # Este metodo es el encargado de preparar los destinatarios, asunto y adjuntos.
   #
   def eemail(email, file_path) 
-    puts "llega al email sender"
-    puts email
-    puts file_path
-    attachments["testeeeeee.pdf"] = File.read(file_path)
-    mail(:to => email, :subject => 'Informe') do |format|
+    file_name = file_path.split("/").last
+    time = Time.new
+    month_number = time.month
+    year = time.year
+    months = {
+      1 => 'Enero', 2 => 'Febrero', 3 => 'Marzo', 4 => 'Abril',
+      5 => 'Mayo', 6 => 'Junio', 7 => 'Julio', 8 => 'Agosto',
+      9 => 'Septiembre', 10 => 'Octubre', 11 => 'Noviembre', 12 => 'Diciembre'}
+    month = months[month_number]
+    attachments[file_name] = File.read(file_path)
+    mail(:to => email, :subject => "Informe #{month} de #{year}") do |format|
       format.text do
-        render :text => 'Este es un test de envio de email'
+        render :text => 'DS Representaciones agradece la preferencia. Por seguridad confirme la recepcion de este email con un ok. Gracias.'
       end
     end 
   end
