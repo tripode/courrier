8aff3bced94a4077ad69516089b540dd20a3e099
wildo <setwildo31@gmail.com>
2012-05-20 11:49:48 -0430

    Issue #123 Zonas: Interfaz que permite agregar zonas

--- project/app/controllers/routing_sheets_controller.rb
+++ project/app/controllers/routing_sheets_controller.rb
@@ -268,7 +268,7 @@ class RoutingSheetsController < ApplicationController
   def search
     @number=params[:number]
     @employee_id=params[:employee_id]
-    @area_id=params[:area_id]
+    @area_id=params[:city_id]
     @state_id=params[:routing_sheet_state_id]
     @date_start=params[:date_start]
     @date_end=params[:date_end]
--- project/app/models/area.rb
+++ project/app/models/area.rb
@@ -1,6 +1,7 @@
 class Area < ActiveRecord::Base
   has_many :routing_sheets, :dependent => :destroy
   belongs_to :city
+  validates :area_name,:city_id, :presence =>true
   
   def getAreas
     Area.all.collect{|a|[a.area_name, a.id]}
