11bd0b985f5d730a952e7c59eb1b07b8b454c3cf
unknown <wildo@wildo-pc.(none)>
2012-06-25 13:06:06 -0500

    #Issue 187 Hoja de Ruta: Modificaciones requeridas al modelo impreso

--- project/app/controllers/routing_sheet_pdf.rb
+++ project/app/controllers/routing_sheet_pdf.rb
@@ -6,37 +6,39 @@ class RoutingSheetPdf< Prawn::Document
     super()
     
     @routing_sheet = routing_sheet
-    text "Hoja de Ruta", :align => :center 
-    text "Nro: #{@routing_sheet.number}"
-    text "Fecha: #{@routing_sheet.date}"
-    text "Repartidor: #{@routing_sheet.employee.last_name + ' '  + @routing_sheet.employee.name}"
-    text "Zona: #{@routing_sheet.area.area_name}"
-    text "OBS: #{@routing_sheet.commentary}"
-    @routing_sheets_details=details.collect{|detail| 
-      [details.index(detail) + 1,
-      detail.product.bar_code,
-      detail.product.product_type.description,
-      if !detail.product.receiver_id.nil? then detail.product.receiver.receiver_name end,
-      if !detail.product.receiver_address_id.nil? then detail.product.receiver_address.address end,
-      detail.who_received,
-      if !detail.reason_id.nil? then detail.reason.description end]
-    }
-    header_data=[["Item","Codigo","Tipo Producto","Destinatario","Direccion","Recibio","Motivo no entrega"]]
-    t=header_data.concat(@routing_sheets_details)
-    t=make_table(header_data,:cell_style => { :font => "Times-Roman", :font_style => :italic, :size => 10})
-    move_down 30
-    t.draw
-    move_down 30
-    text "Firma Mensajero:____________________"
-    move_down 30
-    text "<u><link href='#{root_url}routing_sheets'>Regresar</link></u>", :inline_format => true
-    
-    number_pages "<page> de <total>", 
+     number_pages "PAG:<page>", 
                                          {:start_count_at => 1,
                                           :page_filter => lambda{ |pg| pg != 0 },
-                                          :at => [bounds.right - 50, 0],
+                                          :at => [bounds.right - 50, 678],
                                           :align => :right,
-                                          :size => 14}
+                                          :size => 10}
+    text "Hoja de Ruta", :align => :left 
+    text "DS Representaciones"
+    draw_text "#{routing_sheet.date.strftime("%d-%m-%Y")}", :at => [480, 708]
+    move_down 15
+    text "Nro HOJA DE RUTA: #{@routing_sheet.number}     REPARTIDOR: #{@routing_sheet.employee.last_name + ' '  + @routing_sheet.employee.name}      ZONA: #{@routing_sheet.area.area_name}"
+    
+    @routing_sheets_details=details.collect{|detail| 
+      [
+      detail.product.bar_code,
+      if !detail.product.receiver_id.nil? then detail.product.receiver.receiver_name end  + "\n  " +
+      if !detail.product.receiver_address_id.nil? then detail.product.receiver_address.address end,
+      "_____________________",
+      "___________________________________________"]
+    }
+    header_data=[["CODIGO","DATOS DE USUARIO","FIRMA","ACLARACION"]]
+    t=header_data.concat(@routing_sheets_details)
+    t=make_table(header_data,:cell_style => { :font => "Times-Roman", :font_style => :italic, :size => 10, :border_width => 0 })
+    move_down 30
+    t.draw
+    move_down 10
+    text "TOTAL: #{details.length}"
+    move_down 30
+    text "Firma Mensajero:____________________"
+   
+   
+    
+   
   end
 
 end
