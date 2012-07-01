203215ce2f031c75abc12cc9d1130e6417d3af17
wildo <setwildo31@gmail.com>
2012-07-01 10:11:18 -0430

    Correccion dl header del informe de entrega de cliente

--- project/app/controllers/delivery_report_pdf.rb
+++ project/app/controllers/delivery_report_pdf.rb
@@ -28,10 +28,9 @@ class DeliveryReportPdf< Prawn::Document
       if !detail.product.received_at.nil? then detail.product.received_at.strftime("%d-%m-%Y") else if !RoutingSheet.where(id: detail.routing_sheet_id).first.nil? then RoutingSheet.where(id: detail.routing_sheet_id).first.date.strftime("%d-%m-%Y") end end ]
     }
     header_data=[["Item","Codigo","Destinatario","Direccion","Recibido por","Devuelto por motivo", "Fecha"]]
-    t_header = make_table(header_data,:cell_style => { :font => "Helvetica",:font_style => :bold,:size => 10,:border_width => 0},:column_widths => { 0 => 35, 1 => 85, 2 => 85, 3 => 85, 4 => 80, 5 => 80, 6 => 85, 7 => 80},:header => true)
-    t_header.draw
-    t=make_table(@routing_sheets_details,:cell_style => { :font => "Helvetica", :size => 10,:border_width => 0},:column_widths => { 0 => 35, 1 => 85, 2 => 85, 3 => 85, 4 => 80, 5 => 80, 6 => 85, 7 => 80})  
-    t.draw
+    table(header_data.concat(@routing_sheets_details),:header => true,:cell_style => { :font => "Helvetica", :size => 10,:border_width => 0},:column_widths => { 0 => 35, 1 => 85, 2 => 85, 3 => 85, 4 => 80, 5 => 80, 6 => 85, 7 => 80})  do
+       row(0).font_style = :bold
+    end
     move_down 30
     
     #text "<u><a href='#{root_url}products/send_email?customer_id=#{customer.id}&file_path=#{file_path}' method='post'>Enviar</a></u>   <u><link href='#{url_new}'>Nuevo reporte</link></u>  <u><link href='#{root_url}main_page/index'>Cancelar</link></u> ", :inline_format => true
